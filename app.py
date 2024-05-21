6b453bd9bcbe960e88c405e9e4ddee236cb7dfb5
David Dominguez <dominguezdavid396@gmail.com>
2024-05-21 16:41:06 -0600

    Implementacion del funcionamiento de formulario con n4j

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 import firebase_admin
 from firebase_admin import credentials, db
 
+#import n4j.DriverN4
+from n4j.DriverN4 import *
+
 app = Flask(__name__)
 
 # Inicializa la aplicación de Firebase
@@ -71,7 +74,7 @@ def handle_login():
     name = request.form['name']
     password = request.form['password']
     if verificar_usuario(name, password):
-        return redirect(url_for('preguntas'))
+        return redirect(url_for('preguntas', nombre_usuario=name))
     else:
         return redirect(url_for('login'))  # Redirige al login si la verificación falla
 
@@ -84,7 +87,44 @@ def handle_register():
 
 @app.route('/preguntas')
 def preguntas():
-    return render_template('Preguntas.html')
+    nombre_usuario = request.args.get('nombre_usuario')
+    return render_template('Preguntas.html', nombre_usuario=nombre_usuario)
+
+@app.route('/procesar_preguntas', methods=['POST'])
+def procesar_preguntas():
+    # Captura los datos del formulario
+    nombre_usuario = request.form['usuario']
+    comida = request.form['comida']
+    q1 = request.form['q1']
+    q2 = request.form['q2']
+    q3 = request.form['q3']
+    q4 = request.form['q4']
+    q5 = request.form['q5']
+    rating = request.form['rating']
+    
+    # Llama a otra función pasando los datos como parámetros
+    procesar_respuestas(nombre_usuario, comida, q1, q2, q3, q4, q5, rating)
+    
+    return redirect(url_for('recomendaciones'))
+
+@app.route('/recomendaciones')
+def recomendaciones():
+    return render_template('Recomendaciones.html')
+
+def procesar_respuestas(nombre_usuario, comida, q1, q2, q3, q4, q5, rating):
+    # Aquí puedes procesar las respuestas como desees
+    print(f"Usuario: {nombre_usuario}")
+    print(f"Comida: {comida}")
+    print(f"Q1: {q1}")
+    print(f"Q2: {q2}")
+    print(f"Q3: {q3}")
+    print(f"Q4: {q4}")
+    print(f"Q5: {q5}")
+    print(f"Rating: {rating}")
+
+    # UserN, ComidaN, TempN, SaborN, TexturaN, LugarN, TipoN, RateN
+    example.create_nodes_and_relationships(nombre_usuario,comida,q1,q2,q3,q4,q5,rating)
+    example.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- n4j/DriverN4.py
+++ n4j/DriverN4.py
@@ -1,4 +1,4 @@
-from neo4j import GraphDatabase 
+from neo4j import GraphDatabase
 
 class Neo4j_C:
     global listName
@@ -11,17 +11,20 @@ class Neo4j_C:
 
     def close(self):
         self._driver.close()
-    def create_nodes_and_relationships(self):
+
+    def create_nodes_and_relationships(self, UserN, ComidaN, TempN, SaborN, TexturaN, LugarN, TipoN, RateN):
         with self._driver.session() as session:
-            session.write_transaction(self._create_data)
+            session.write_transaction(self._create_data, UserN, ComidaN, TempN, SaborN, TexturaN, LugarN, TipoN, RateN)
+
     def mostrar_datos(self):
         with self._driver.session() as session:
             names = session.read_transaction(self._view_data)
             for i in range(len(names)):
                 listName.append(names[i])
-    def recomendation_comida(usuario_activo):
+
+    def recomendation_comida(self, usuario_activo):
         if usuario_activo:
-            example.recomendar_comida(usuario_activo)
+            self.recomendar_comida(usuario_activo)
         else:
             print("Por favor, ingrese sus preferencias primero.")
 
@@ -31,26 +34,13 @@ class Neo4j_C:
             print(f"Recomendaciones para {user_name}:")
             for nodo in recommendations:
                 reclist.append(nodo)
-
         print(reclist)
-
 
     # Nodos y sus relaciones
     @staticmethod
-    def _create_data(tx):
-        # Definir las variables
-        UserN = "David"
-        ComidaN = "Lasagna"
-        TempN = "Templada"
-        SaborN = "Salado"
-        TexturaN = "Solido"
-        LugarN = "Casa"
-        TipoN = "Chatarra"
-        RateN = "9"
-
+    def _create_data(tx, UserN, ComidaN, TempN, SaborN, TexturaN, LugarN, TipoN, RateN):
         global listName
 
-        print(listName, "***")
         # Crear nodos de usuario y comida con parámetros
         tx.run("CREATE (:User {name: $UserN})", UserN=UserN)
 
@@ -59,94 +49,93 @@ class Neo4j_C:
             tx.run("CREATE (:Comida {name: $ComidaN})", ComidaN=ComidaN)
         else:
             tx.run("MATCH (c:User {name: $UserN}), (t:Comida {name: $ComidaN}) "
-            "MERGE (c)-[:PERTENECE]->(t)", UserN=UserN, ComidaN=ComidaN)
+                   "MERGE (c)-[:PERTENECE]->(t)", UserN=UserN, ComidaN=ComidaN)
         if TempN not in listName:
             tx.run("CREATE (:Temperatura {name: $TempN})", TempN=TempN)
         else:
             tx.run("MATCH (c:Comida {name: $ComidaN}), (t:Temperatura {name: $TempN}) "
-            "MERGE (c)-[:PERTENECE]->(t)", ComidaN=ComidaN, TempN=TempN)
+                   "MERGE (c)-[:PERTENECE]->(t)", ComidaN=ComidaN, TempN=TempN)
         if SaborN not in listName:
             tx.run("CREATE (:Sabor {name: $SaborN})", SaborN=SaborN)
         else:
             tx.run("MATCH (c:Comida {name: $ComidaN}), (s:Sabor {name: $SaborN}) "
-            "MERGE (c)-[:PERTENECE]->(s)", ComidaN=ComidaN, SaborN=SaborN)
+                   "MERGE (c)-[:PERTENECE]->(s)", ComidaN=ComidaN, SaborN=SaborN)
         if TexturaN not in listName:
             tx.run("CREATE (:Textura {name: $TexturaN})", TexturaN=TexturaN)
         else:
             tx.run("MATCH (c:Comida {name: $ComidaN}), (tx:Textura {name: $TexturaN}) "
-            "MERGE (c)-[:PERTENECE]->(tx)", ComidaN=ComidaN, TexturaN=TexturaN)
+                   "MERGE (c)-[:PERTENECE]->(tx)", ComidaN=ComidaN, TexturaN=TexturaN)
         if LugarN not in listName:
             tx.run("CREATE (:Lugar {name: $LugarN})", LugarN=LugarN)
         else:
             tx.run("MATCH (c:Comida {name: $ComidaN}), (l:Lugar {name: $LugarN}) "
-            "MERGE (c)-[:PERTENECE]->(l)", ComidaN=ComidaN, LugarN=LugarN)
+                   "MERGE (c)-[:PERTENECE]->(l)", ComidaN=ComidaN, LugarN=LugarN)
         if TipoN not in listName:
             tx.run("CREATE (:Tipo {name: $TipoN})", TipoN=TipoN)
         else:
             tx.run("MATCH (c:Comida {name: $ComidaN}), (tp:Tipo {name: $TipoN}) "
-            "MERGE (c)-[:PERTENECE]->(tp)", ComidaN=ComidaN, TipoN=TipoN)
+                   "MERGE (c)-[:PERTENECE]->(tp)", ComidaN=ComidaN, TipoN=TipoN)
         if RateN not in listName:
             tx.run("CREATE (:Rate {name: $RateN})", RateN=RateN)
         else:
             tx.run("MATCH (c:Comida {name: $ComidaN}), (r:Rate {name: $RateN}) "
-            "MERGE (c)-[:TIENE]->(r)", ComidaN=ComidaN, RateN=RateN)
+                   "MERGE (c)-[:TIENE]->(r)", ComidaN=ComidaN, RateN=RateN)
 
         # Establecer relaciones entre usuario y comida
         tx.run("MATCH (u:User {name: $UserN}), (c:Comida {name: $ComidaN}) "
-            "MERGE (u)-[:WATCH]->(c)", UserN=UserN, ComidaN=ComidaN)
-        
+               "MERGE (u)-[:WATCH]->(c)", UserN=UserN, ComidaN=ComidaN)
+
         # Establecer relaciones entre comida y características
         tx.run("MATCH (c:Comida {name: $ComidaN}), "
-            "(t:Temperatura {name: $TempN}), "
-            "(s:Sabor {name: $SaborN}), "
-            "(tx:Textura {name: $TexturaN}), "
-            "(l:Lugar {name: $LugarN}), "
-            "(tp:Tipo {name: $TipoN}) "
-            "MERGE (c)-[:PERTENECE]->(t) "
-            "MERGE (c)-[:PERTENECE]->(s) "
-            "MERGE (c)-[:PERTENECE]->(tx) "
-            "MERGE (c)-[:PERTENECE]->(l) "
-            "MERGE (c)-[:PERTENECE]->(tp)",
-            ComidaN=ComidaN, TempN=TempN, SaborN=SaborN, TexturaN=TexturaN, LugarN=LugarN, TipoN=TipoN)
+               "(t:Temperatura {name: $TempN}), "
+               "(s:Sabor {name: $SaborN}), "
+               "(tx:Textura {name: $TexturaN}), "
+               "(l:Lugar {name: $LugarN}), "
+               "(tp:Tipo {name: $TipoN}) "
+               "MERGE (c)-[:PERTENECE]->(t) "
+               "MERGE (c)-[:PERTENECE]->(s) "
+               "MERGE (c)-[:PERTENECE]->(tx) "
+               "MERGE (c)-[:PERTENECE]->(l) "
+               "MERGE (c)-[:PERTENECE]->(tp)",
+               ComidaN=ComidaN, TempN=TempN, SaborN=SaborN, TexturaN=TexturaN, LugarN=LugarN, TipoN=TipoN)
+
         # Establecer relación entre comida y valoración (rating)
         tx.run("MATCH (c:Comida {name: $ComidaN}), (r:Rate {name: $RateN}) "
-            "MERGE (c)-[:TIENE]->(r)", ComidaN=ComidaN, RateN=RateN)
-        
+               "MERGE (c)-[:TIENE]->(r)", ComidaN=ComidaN, RateN=RateN)
+
     @staticmethod
     def _view_data(tx):
         result = tx.run("MATCH (n) RETURN n.name AS name")
         names = [record["name"] for record in result]
         return names
-    
+
     @staticmethod
     def _view_food(tx):
         result = tx.run("MATCH (n:Comida) RETURN n.name AS name")
         names = [record["name"] for record in result]
         return names
-    
 
-
-@staticmethod
-def _recommend_food(tx, user_name):
-    query = """
-    MATCH (u:User {name: $user_name})-[:WATCH]->(f:Comida)
-    MATCH (f)-[:PERTENECE]->(t:Temperatura)
-    MATCH (f)-[:PERTENECE]->(s:Sabor)
-    MATCH (f)-[:PERTENECE]->(tx:Textura)
-    MATCH (f)-[:PERTENECE]->(l:Lugar)
-    MATCH (f)-[:PERTENECE]->(tp:Tipo)
-    WITH t, s, tx, l, tp
-    MATCH (otherFoods:Comida)
-    WHERE (otherFoods)-[:PERTENECE]->(t) ANd
-          (otherFoods)-[:PERTENECE]->(s) AND
-          (otherFoods)-[:PERTENECE]->(tx) AND
-          (otherFoods)-[:PERTENECE]->(l) AND
-          (otherFoods)-[:PERTENECE]->(tp) AND
-          (otherFoods) <> f
-    RETURN otherFoods.name AS name
-    """
-    result = tx.run(query, user_name=user_name)
-    return [record["name"] for record in result]
+    @staticmethod
+    def _recommend_food(tx, user_name):
+        query = """
+        MATCH (u:User {name: $user_name})-[:WATCH]->(f:Comida)
+        MATCH (f)-[:PERTENECE]->(t:Temperatura)
+        MATCH (f)-[:PERTENECE]->(s:Sabor)
+        MATCH (f)-[:PERTENECE]->(tx:Textura)
+        MATCH (f)-[:PERTENECE]->(l:Lugar)
+        MATCH (f)-[:PERTENECE]->(tp:Tipo)
+        WITH t, s, tx, l, tp
+        MATCH (otherFoods:Comida)
+        WHERE (otherFoods)-[:PERTENECE]->(t) AND
+              (otherFoods)-[:PERTENECE]->(s) AND
+              (otherFoods)-[:PERTENECE]->(tx) AND
+              (otherFoods)-[:PERTENECE]->(l) AND
+              (otherFoods)-[:PERTENECE]->(tp) AND
+              (otherFoods) <> f
+        RETURN otherFoods.name AS name
+        """
+        result = tx.run(query, user_name=user_name)
+        return [record["name"] for record in result]
 
 
 # Configuración de conexión y ejecución de la creación de datos
@@ -156,9 +145,11 @@ password = "0iIoDYVv8wV4MZIcF_405l5muLHL3mMjuXN2tUevJ_w"
 
 # Crear una instancia de la clase y ejecutar la creación de nodos y relaciones
 example = Neo4j_C(uri, user, password)
-example.mostrar_datos()
-example.create_nodes_and_relationships()
-example.recomendar_comida("David")
+# Se pasan los parámetros deseados
+#example.create_nodes_and_relationships("Pepito", "Helado", "Frio", "Dulce", "Cremoso", "Restaurante", "Chatarra", "10")
+
+#example.mostrar_datos()
+#example.recomendar_comida("David")
 
 # Cerrar la conexión al finalizar
-example.close()
+#example.close()
